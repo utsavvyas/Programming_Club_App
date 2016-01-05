3bfbcc501bb8edafe81be6e8e12a672373f187b0
Rajdeep Pinge <rajpinge@gmail.com>
2016-01-05 15:05:48 +0530

    Improved Programming club app

---
+++ app/src/main/java/programmingclub/daiict/CategoryListView.java
@@ -0,0 +1,69 @@
+package programmingclub.daiict;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class CategoryListView extends MainActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+
+        //trying to get the drawer here as well
+        frameLayout = (FrameLayout)findViewById(R.id.content_frame);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        //mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, listArray));
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                openActivity(position);
+            }
+        });
+
+
+        final MySQLiteHelper mySQLiteHelper = new MySQLiteHelper(this);
+        final ListView list = (ListView)findViewById(R.id.card_listViewCategory);
+        List<String> category = mySQLiteHelper.getCategories();
+        System.out.print(category.size());
+        CategoryCardAdapter cardArrayAdapter = new CategoryCardAdapter(this.getApplicationContext(), R.layout.category_card_layout);
+        for(int i=0;i<category.size();i++) {
+            Card c = new Card(category.get(i), "");
+            System.out.println(c.getLine1() + "dfdfdsfff");
+            cardArrayAdapter.add(c);
+        }
+        list.setAdapter(cardArrayAdapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent in=new Intent("programmingclub.daiict.BLOGCATEGORYLISTVIEW");
+                Bundle b = new Bundle();
+                b.putString("category", ((Card) list.getItemAtPosition(position)).getLine1());
+                in.putExtras(b);
+                startActivity(in);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/programmingclub/daiict/MainActivity.java
@@ -0,0 +1,315 @@
+package programmingclub.daiict;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+//AndroidManifest, combine MainActivities
+
+
+/**
+ * @author dipenp
+ *
+ * This activity will add Navigation Drawer for our application and all the code related to navigation drawer.
+ * We are going to extend all our other activites from this BaseActivity so that every activity will have Navigation Drawer in it.
+ * This activity layout contain one frame layout in which we will add our child activity layout.
+ */
+public class MainActivity extends Activity {
+
+    /**
+     *  Frame layout: Which is going to be used as parent layout for child activity layout.
+     *  This layout is protected so that child activity can access this
+     *  */
+    protected FrameLayout frameLayout;
+
+    /**
+     * ListView to add navigation drawer item in it.
+     * We have made it protected to access it in child class. We will just use it in child class to make item selected according to activity opened.
+     */
+
+    protected ListView mDrawerList;
+
+    /**
+     * List item array for navigation drawer items.
+     * */
+    protected String[] listArray = { "Blog", "Events", "Announcements", "Editorial", "Tech News" };
+
+    /**
+     * Static variable for selected item position. Which can be used in child activity to know which item is selected from the list.
+     * */
+    protected static int position;
+
+    /**
+     *  This flag is used just to check that launcher activity is called first time
+     *  so that we can open appropriate Activity on launch and make list item position selected accordingly.
+     * */
+    private static boolean isLaunch = true;
+
+    /**
+     *  Base layout node of this Activity.
+     * */
+     DrawerLayout mDrawerLayout;
+
+    /**
+     * Drawer listner class for drawer open, close etc.
+     */
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        WebView w = (WebView) findViewById(R.id.webView);
+
+        final asyncex as = new asyncex();
+        // String param[] = new String[3];
+        try {
+            String result = as.execute("https://public-api.wordpress.com/rest/v1.1/sites/omkarsiteblog.wordpress.com/posts/5").get();
+            JSONObject jobj = new JSONObject(result);
+            //     jArray = jobj.getJSONArray("posts");
+
+            String s = (String)jobj.get("content");
+            String parts[]=s.split("\"");
+            String url=parts[1];
+            w.loadUrl(url);
+
+            //Log.d("url",url);
+
+        }
+        catch (Exception e) {
+            // TODO Auto-generated catch block
+            Toast.makeText(this.getApplicationContext(), "No Internet Connection.Please connect to Internet", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+        frameLayout = (FrameLayout)findViewById(R.id.content_frame);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        //mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        // set up the drawer's list view with items and click listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, listArray));
+        mDrawerList.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                openActivity(position);
+            }
+        });
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions between the sliding drawer and the action bar app icon
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,						/* host Activity */
+                mDrawerLayout, 				/* DrawerLayout object */
+                R.drawable.ic_launcher,     /* nav drawer image to replace 'Up' caret */
+                R.string.open_drawer,       /* "open drawer" description for accessibility */
+                R.string.close_drawer)      /* "close drawer" description for accessibility */
+        {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+                if (position>=0)
+                getActionBar().setTitle(listArray[position]);
+
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(getString(R.string.app_name));
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                super.onDrawerStateChanged(newState);
+            }
+        };
+        mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+
+        /**
+         * As we are calling BaseActivity from manifest file and this base activity is intended just to add navigation drawer in our app.
+         * We have to open some activity with layout on launch. So we are checking if this BaseActivity is called first time then we are opening our first activity.
+         * */
+        if(isLaunch){
+            /**
+             *Setting this flag false so that next time it will not open our first activity.
+             *We have to use this flag because we are using this BaseActivity as parent activity to our other activity.
+             *In this case this base activity will always be call when any child activity will launch.
+             */
+            isLaunch = false;
+            openActivity(-5);
+        }
+    }
+
+    //inner class for retrive data from wordpress rest api
+    public class asyncex extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... arg0) {
+            // TODO Auto-generated method stub.
+            String response = "";
+            String url = arg0[0];
+
+            try {
+                URL obj = new URL(url);
+                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+                // optional default is GET
+                con.setRequestMethod("GET");
+
+                //add request header
+                con.setRequestProperty("Accept", "text/json");
+
+                int responseCode = con.getResponseCode();
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+
+                while ((inputLine = in.readLine()) != null) {
+                    response += inputLine;
+                }
+                in.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+    }
+
+    /**
+     * @param position
+     *
+     * Launching activity when any list item is clicked.
+     */
+    protected void openActivity(int position) {
+
+        /**
+         * We can set title & itemChecked here but as this BaseActivity is parent for other activity,
+         * So whenever any activity is going to launch this BaseActivity is also going to be called and
+         * it will reset this value because of initialization in onCreate method.
+         * So that we are setting this in child activity.
+         */
+//		mDrawerList.setItemChecked(position, true);
+//		setTitle(listArray[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+        MainActivity.position = position; //Setting currently selected position in this field so that it will be available in our child activities.
+
+        switch (position) {
+            case 0:
+                startActivity(new Intent(this, BlogListView.class));
+                break;
+            case 1:
+                startActivity(new Intent(this, EventListView.class));
+                break;
+            case 2:
+                startActivity(new Intent(this, AnnouncementListView.class));
+                break;
+            case 3:
+                startActivity(new Intent(this, CategoryListView.class)); //where is the editorial
+                break;
+            case 4:
+                startActivity(new Intent(this, MainActivity_RSS.class));
+                break;
+
+            default:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+
+       // Toast.makeText(this, "Selected Item Position::"+position, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /* Called whenever we call invalidateOptionsMenu() */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    /* We can override onBackPressed method to toggle navigation drawer*/
+    @Override
+    public void onBackPressed() {
+
+       // Intent intent = new Intent(this,MainActivity.class); //I explicity instruct the activity to launch main acitivity and close itself
+       //startActivity(intent);
+        position=-5;
+        getActionBar().setTitle(getString(R.string.app_name));
+        finish();
+        if(mDrawerLayout.isDrawerOpen(mDrawerList)){
+            mDrawerLayout.closeDrawer(mDrawerList);
+
+
+
+            finish();
+
+        }else {
+            mDrawerLayout.openDrawer(mDrawerList);
+
+        }
+    }
+}
